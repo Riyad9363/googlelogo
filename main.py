9d463239793abd1c1d848c9696edd27cce74796e
Shahria Hussain Riyad <shahriahussainriyad@Shahrias-MacBook-Air.local>
2022-10-06 17:57:05 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import turtle
+
+codingplex = turtle.Turtle()
+turtle.bgcolor("black")
+codingplex.color("#4285F4", "#4285F4")
+codingplex.pensize(1)
+
+
+
+def google():
+    codingplex.forward(120)
+    codingplex.right(90)
+    codingplex.circle(-150, 50)
+    codingplex.color("#0F9D58")
+    codingplex.circle(-150, 100)
+    codingplex.color("#F4B400")
+    codingplex.circle(-150, 60)
+    codingplex.color("#DB4437", "#DB4437")
+    codingplex.begin_fill()
+    codingplex.circle(-150, 100)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.right(90)
+    codingplex.circle(100, 100)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.end_fill()
+    codingplex.begin_fill()
+    codingplex.color("#F4B400", "#F4B400")
+    codingplex.right(180)
+    codingplex.forward(50)
+    codingplex.right(90)
+    codingplex.circle(100, 60)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.right(90)
+    codingplex.circle(-150, 60)
+    codingplex.end_fill()
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.right(90)
+    codingplex.circle(100, 60)
+    codingplex.color("#0F9D58", "#0F9D58")
+    codingplex.begin_fill()
+    codingplex.circle(100, 100)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.right(90)
+    codingplex.circle(-150, 100)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.end_fill()
+    codingplex.right(90)
+    codingplex.circle(100, 100)
+    codingplex.color("#4285F4", "#4285F4")
+    codingplex.begin_fill()
+    codingplex.circle(100, 25)
+    codingplex.left(115)
+    codingplex.forward(65)
+    codingplex.right(90)
+    codingplex.forward(42)
+    codingplex.right(90)
+    codingplex.forward(124)
+    codingplex.right(90)
+    codingplex.circle(-150, 50)
+    codingplex.right(90)
+    codingplex.forward(50)
+    codingplex.end_fill()
+
+
+google()
+codingplex.hideturtle()
+turtle.done()
+
+#Code By Riyad
+
+# [ google color codes ]
+#   red    = #DB4437
+#   blue   = #4285F4
+#   green  = #0F9D58
+#   yellow = #F4B400
